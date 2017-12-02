7c8ad15e49faa6630a19f59d7c553e788fa90004
Gaurav kumar <Kumargaurav1137@gmail.com>
2017-12-02 15:51:04 +0530

    Initial mock server commit 2

--- app/src/main/java/org/jbossoutreach/restandroid/activities/MainActivity.java
+++ app/src/main/java/org/jbossoutreach/restandroid/activities/MainActivity.java
@@ -5,25 +5,31 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.jbossoutreach.restandroid.R;
 import org.jbossoutreach.restandroid.backend.RestClient;
 import org.jbossoutreach.restandroid.model.GitRepo;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import retrofit2.Call;
 import retrofit2.Callback;
 
 public class MainActivity extends AppCompatActivity{
 
-    private Button getTeacher;
+    private Button getRepo;
+    private TextView titleTextView,prTextView,issueTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getTeacher = (Button) findViewById(R.id.get_teacher_by_id);
+        getRepo = (Button) findViewById(R.id.get_repo_by_title);
+        titleTextView = (TextView) findViewById(R.id.repo_title);
+        issueTextView = (TextView) findViewById(R.id.issue_count);
+        prTextView = (TextView) findViewById(R.id.pr_count);
 
         ButterKnife.bind(this);
 
@@ -40,18 +46,11 @@ public class MainActivity extends AppCompatActivity{
 //
 //            }
 //        });
-        getTeacher.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                getTeacher(view);
-            }
-        });
     }
 
 
-
-    public void getTeacher(View view){
+   @OnClick(R.id.get_repo_by_title)
+    public void getGitRepo(View view){
         Call<GitRepo> repoCall = RestClient.getClient()
                 .getRepoByTitle("GCI");
 
@@ -59,12 +58,15 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onResponse(Call<GitRepo> call, retrofit2.Response<GitRepo> response) {
                 GitRepo gitRepo = response.body();
-                Log.d("Teacher Response",gitRepo.toString());
+                titleTextView.setText("Title : "+gitRepo.getRepoTitle());
+                issueTextView.setText("Issue Count : "+gitRepo.getIssueCount());
+                prTextView.setText("PR Count : "+gitRepo.getPrCount());
+                Log.d("GitRepo Response",gitRepo.toString());
             }
 
             @Override
             public void onFailure(Call<GitRepo> call, Throwable t) {
-                Log.d("Teacher Failure", "Failure " + t.getMessage());
+                Log.d("GitRepo Failure", "Failure " + t.getMessage());
             }
         });
 
